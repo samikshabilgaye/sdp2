34a58e31303a9e5b83d2558bcc6fffa97e6fd0b8
samikshabilgaye <samikshabilgaye9@gmail.com>
2026-02-21 10:32:02 +0530

    Create StudentRepository interface for Student entity

---
+++ backend/src/main/java/com/studentmanagement/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.studentmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.studentmanagement.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    // Additional query methods can be defined here
+}
